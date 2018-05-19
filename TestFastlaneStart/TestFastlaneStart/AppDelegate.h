5d36493c84e1db9dfc22f847db4393be4706cd42
tankxie <shoutanxie@gmail.com>
2018-05-19 17:18:31 +0800

    [Task] init commit.
    [Task] init commit.

---
+++ TestFastlaneStart/TestFastlaneStart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFastlaneStart
+//
+//  Created by tank on 2018/5/19.
+//  Copyright © 2018 xxxxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFastlaneStart/TestFastlaneStart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFastlaneStart
+//
+//  Created by tank on 2018/5/19.
+//  Copyright © 2018 xxxxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
